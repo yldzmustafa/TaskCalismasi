617a5753150800b39d256f143a1326a7c34beb6e
MUSTAFA <93928382+yldzmustafa@users.noreply.github.com>
2024-02-29 00:02:59 +0300

    inputlardan props gönderme işlemi tamamlandı.

--- jiraaplication/src/components/TaskCreate.js
+++ jiraaplication/src/components/TaskCreate.js
@@ -1,13 +1,34 @@
-function TaskCreate() {
+import {useState} from 'react'
+
+function TaskCreate({onCreate}) {
+
+    const [title, setTitle]         = useState('')
+    const [taskDesc, setTaskDesc]   = useState('')
+
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleTaskChange = (event) => {
+        setTaskDesc(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title, taskDesc)
+        setTitle('')
+        setTaskDesc('')
+    }
+
     return ( 
         <div className="taskCreateDiv">
             <h3>Lütfen Task Ekleyiniz</h3>
-            <form className="taskForm">
+            <form onSubmit={handleSubmit} className="taskForm">
                 <label className="taskLabel">Başlık</label>
-                <input className="taskInput"/>
+                <input value={title} onChange={handleChange} className="taskInput"/>
                 <label className="taskLabel">Task Giriniz!</label>
-                <textarea className="taskInput" rows={5}></textarea>
-                <button className="taskButton">Oluştur</button>
+                <textarea value={taskDesc} onChange={handleTaskChange} className="taskInput" rows={5}></textarea>
+                <button onClick={handleSubmit} className="taskButton">Oluştur</button>
             </form>
         </div> );
 }
